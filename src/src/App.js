9a66935c13627f2cf36edc2e57a2ed1575c3aa59
Mr Scary <ronwazny@gmail.com>
2019-04-02 19:15:10 -0400

    Clicking

--- src/src/App.js
+++ src/src/App.js
@@ -1,96 +1,84 @@
-import React, { Component } from "react";
-import FriendCard from "./components/FriendCard";
-// import Jumbotron from "./components/Jumbotron";
-import Footer from "./components/Footer";
-import Title from "./components/Title";
-import friends from "./friends.json";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-
-
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import Title from './components/Title';
+import FriendCard from './components/FriendCard';
+import friends from './friends.json';
+import Footer from './components/Footer';
+// import Jumbotron from './components/Jumbotron';
+// import Score from './components/score';
+// import './App.css';
+import Wrapper from './components/Wrapper';
 
 class App extends Component {
   // Setting this.state.RB to 0 or empty
   state = {
     friends,
-     clickedFriend: [],
-    score: 0
+    clickedFriend: [],
+    score: 0,
+    restart: false
   };
 
-//when you click on a card ... the RB is taken out of the array
-imageClick = event => {
-  const currentFriend = event.target.alt;
-  const friendAlreadyClicked =
-    this.state.clickedFriend.indexOf(currentFriend) > -1;
+  //when you click on a card ... the RB is taken out of the array
+  imageClick = event => {
+    const currentFriend = event.target.alt;
+    const clickedFriend = this.state.clickedFriend;
 
-//if you click on a RB that has already been selected, the game is reset and cards reordered
-if (friendAlreadyClicked) {
-  this.setState({
-    friend: this.state.friend.sort(function(a, b) {
-      return 0.5 - Math.random();
-    }),
-    clickedFriend: [],
-    score: 0
-  });
-    alert("You Got Juked!! Play again?");
+    var RB = this.state.clickedFriend.indexOf(currentFriend) > -1;
+    if (RB === false) {
+      clickedFriend.push(currentFriend);
+    } else {
+      this.setState({
+        friends: this.state.friends.sort(function(a, b) {
+          return Math.random() - 0.5;
+        }),
+        clickedFriend: [],
+        score: 0
+      });
+      alert('You Got Juked!!');
+      return;
+      //if you click on an available RB, your score is increased and cards reordered
+    }
+    this.setState({
+      friends: this.state.friends.sort(function(a, b) {
+        return Math.random() - 0.5;
+      }),
+      clickedFriend,
+      score: this.state.score + 1
+    });
 
-    //if you click on an available RB, your score is increased and cards reordered
-  } else {
-    this.setState(
-      {
+    if (this.state.score === 16) {
+      alert('TOUCHDOWN!!! Go For Two?');
+      this.setState({
         friend: this.state.friend.sort(function(a, b) {
           return 0.5 - Math.random();
         }),
-        clickedFriend: this.state.clickedFriend.concat(
-          currentFriend
-        ),
-        score: this.state.score + 1
-      },
-     
-      //if you get all 16 RBs correct you get a congrats message and the game resets        
-      () => {
-        if (this.state.score === 16) {
-          alert("TOUCHDOWN! You Win!");
-          this.setState({
-            friend: this.state.friend.sort(function(a, b) {
-              return 0.5 - Math.random();
-            }),
-            clickedFriend: [],
-            score: 0
-          });
-        }
-      }
-    );
-  }
-};
+        clickedFriend: [],
+        score: 0
+      });
+    }
+  };
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <div>
-        <Navbar
-        score={this.state.score}
-       /> 
-       <Wrapper>
-       {/* <Jumbotron /> */}
-      < div className="wrapper">
-        <Title>Running Wild</Title>
-        {this.state.friends.map(friend => (
-          <FriendCard
-            imageClick={this.imageClick}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-            image={friend.image}
-            occupation={friend.occupation}
-            location={friend.location}
-          />
-        ))}
-        </div>
-      <Footer />
-      </Wrapper>
+        <Navbar score={this.state.score} />
+        <Title />
+        <Wrapper>
+          {this.state.friends.map(friend => (
+            <FriendCard
+              imageClick={this.imageClick}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              // occupation={friend.occupation}
+              // location={friend.location}
+            />
+          ))}
+        </Wrapper>
+        <Footer />
       </div>
-      
     );
   }
 }
--- src/src/components/FriendCard/index.js
+++ src/src/components/FriendCard/index.js
@@ -1,27 +1,23 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 
 function FriendCard(props) {
   return (
     <div className="card">
       <div className="img-container">
-        <img alt={props.name} src={props.image} />
+        <img alt={props.name} src={props.image} onClick={props.imageClick} />
       </div>
-      <div className="content">
+      {/* <div className="content">
         <ul>
           <li>
             <strong>Name:</strong> {props.name}
-          </li>
-          <li>
             <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
             <strong>Location:</strong> {props.location}
           </li>
         </ul>
-      </div>
+      </div> */}
       {/* <span onClick={() => props.removeFriend(props.id)} className="remove"> */}
-        {/* 𝘅 */}
+      {/* 𝘅 */}
       {/* </span> */}
     </div>
   );
